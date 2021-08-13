899ea8da1f2eb655afd8141bfbfa6d9565fb72e8
Eneas <eneas.rodrigues25@gmail.com>
2021-08-12 22:09:13 -0300

    fix configurado arquitetura do projeto

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(name='ink-core-forge',
       packages=['ink.core.forge'],
       namespace_packages=['ink', 'ink.core'],
       install_requires=[d for d in DEPENDENCIES if '://' not in d],
-      package_data={'ink.core.forge': ['forge/*']},
+      package_data={'ink.core.forge': ['joins/*']},
       zip_safe=False)
