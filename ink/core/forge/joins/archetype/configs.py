60ee7238a3945c0e3c9e45f7b0d89110ad3cf7a4
Eneas <eneas.rodrigues25@gmail.com>
2021-10-16 17:24:50 -0300

    fix path

--- ink/core/forge/joins/archetype/configs.py
+++ ink/core/forge/joins/archetype/configs.py
@@ -3,7 +3,7 @@ import os
 from ink.core.forge.joins.core.configs import Config, Test
 
 HOME_DIR = os.path.expanduser('~')
-CONFIG_DIR = os.getenv('CONFIG_DIR', os.path.join(HOME_DIR, 'ink'))
+CONFIG_DIR = os.getenv('CONFIG_DIR', os.path.join(HOME_DIR, 'dna'))
 ENV = os.getenv('ENV', 'local')
 
 config = Config(env=ENV, config_dir=CONFIG_DIR)
